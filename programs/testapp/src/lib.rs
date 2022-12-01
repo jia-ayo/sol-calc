1341af869ec57f9565fe71a71d2521121e4f25ee
jia-ayo <milkjia11@gmail.com>
2022-12-01 23:00:47 +0100

    write function

--- programs/testapp/src/lib.rs
+++ programs/testapp/src/lib.rs
@@ -6,10 +6,25 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod testapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer=user, space=246)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_programe: Program<'info, System>
+}
+
+#[account]
+pub struct Calculator{
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64
+}
