25d87f14252dcf9d07f6e8b49f47c00bac7ca3f8
jia-ayo <milkjia11@gmail.com>
2022-12-02 23:01:04 +0100

    done

--- programs/testapp/src/lib.rs
+++ programs/testapp/src/lib.rs
@@ -1,26 +1,30 @@
+use anchor_lang::{prelude::Program, system_program::System};
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod testapp {
     use super::*;
-
     pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.greeting = init_message;
         Ok(())
     }
+
 }
 
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer=user, space=246)]
+pub struct Create<'info>{
+    #[account(init, payer=user, space=264)]
     pub calculator: Account<'info, Calculator>,
     #[account(mut)]
     pub user: Signer<'info>,
-    pub system_programe: Program<'info, System>
+    pub system_program: Program<'info, System>
 }
+
 
 #[account]
 pub struct Calculator{
--- tests/testapp.ts
+++ tests/testapp.ts
@@ -1,16 +1,26 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Testapp } from "../target/types/testapp";
+import assert from 'assert';
+import * as anchor from '@project-serum/anchor';
+import { AnchorProvider, web3 } from '@project-serum/anchor';
+const { SystemProgram } = web3;
 
-describe("testapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe('testapp', () => {
+  const provider = AnchorProvider.local();
+  anchor.setProvider(provider);
+  const calculator = anchor.web3.Keypair.generate();
+  const program = anchor.workspace.Testapp;
 
-  const program = anchor.workspace.Testapp as Program<Testapp>;
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it('creates a calculator', async () => {
+    await program.rpc.create("welcome to Calc", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [calculator],
+    });
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+    assert.ok(account.greeting === "welcome to Calc");
   });
 });
